19be4566f9063ed182f26f0d1d31a578e861bc21
Sudhanshu <sudhanshusongire26@gmail.com>
2025-04-08 13:20:55 +0530

    Navbar changes

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,18 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function MyNavbar() {
+  const navigate = useNavigate();
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Feedback form</Navbar.Brand>
+        <Navbar.Brand onClick={() => navigate('/')}>Feedback Form</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/Admin">Admin</Nav.Link>
+            <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
+            <Nav.Link onClick={() => navigate('/admin')}>Admin</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
